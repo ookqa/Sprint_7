4da19d7111e69e6e04d6152042bed1adbca714e1
Alex Q <okiaminqa@yandex.ru>
2024-03-12 20:17:24 +0100

    fix urls

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 class Urls:
-    COURIER_LOGIN_URL = 'https://qa-scooter.praktikum-services.ru/api/v1/courier/login'
-    COURIER_MAIN_URL = 'https://qa-scooter.praktikum-services.ru/api/v1/courier/'
-    ORDER_MAIN_URL = 'https://qa-scooter.praktikum-services.ru/api/v1/orders'
-    ORDER_TRACK_URL = 'https://qa-scooter.praktikum-services.ru/api/v1/orders/track'
+    BASE_URL = 'https://qa-scooter.praktikum-services.ru/'
+    COURIER_LOGIN_URL = f'{BASE_URL}api/v1/courier/login'
+    COURIER_MAIN_URL = f'{BASE_URL}api/v1/courier/'
+    ORDER_MAIN_URL = f'{BASE_URL}api/v1/orders'
+    ORDER_TRACK_URL = f'{BASE_URL}api/v1/orders/track'
